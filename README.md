5745248b993f93a18b91be7416b83866f254af9b
Alexey Ryabov <6006l1k@gmail.com>
2019-02-21 20:49:31 +0300

    Solve task

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Ближайшие бары
 
-[FIXME. Здесь будет описание проекта]
+Скрипт, позволяющий найти:
+- Самый большой бар;
+- Самый маленький бар;
+- Самый близкий бар (необходимо ввести gps-координаты).
+
+Перед запуском необходимо скачать список баров https://devman.org/fshare/1503831681/4/
 
 # Как запустить
 
@@ -10,9 +15,11 @@
 
 ```bash
 
-$ python bars.py # possibly requires call of python3 executive instead of just python
-# FIXME вывести пример ответа скрипта
-
+$ python bars.py bars.json # possibly requires call of python3 executive instead of just python
+Please input your location (lat, lon): 55.770507, 37.613110
+Closest bar: ПИВНОЙ БАР ЭЛЕФАНТ Address: 2-й Колобовский переулок, дом 12
+Biggest bar: Спорт бар «Красная машина» Seats count: 450
+Smallest bar: БАР. СОКИ Seats count: 0
 ```
 
 Запуск на Windows происходит аналогично.
--- bars.py
+++ bars.py
@@ -1,21 +1,66 @@
 import json
+import sys
+from geopy.distance import distance
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r', encoding="utf8") as file_handler:
+        return json.load(file_handler)
 
 
 def get_biggest_bar(data):
-    pass
+    features = data['features']
+    return max(features,
+               key=lambda kv: kv['properties']['Attributes']['SeatsCount'])
 
 
 def get_smallest_bar(data):
-    pass
+    features = data['features']
+    return min(features,
+               key=lambda kv: kv['properties']['Attributes']['SeatsCount'])
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    features = data['features']
+    user_loc = (latitude, longitude)
+
+    def sort_by_distance(kv):
+        (lon, lat) = kv['geometry']['coordinates']
+        bar_loc = (lat, lon)
+        return distance(bar_loc, user_loc).km
+
+    sorted_by_distance = sorted(features, key=sort_by_distance)
+    return sorted_by_distance[0]
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) != 2:
+        sys.exit("Invalid command line")
+
+    path = sys.argv[1]
+    try:
+        bars = load_data(path)
+    except FileNotFoundError:
+        sys.exit("File '{}' not found".format(path))
+    except json.JSONDecodeError:
+        sys.exit("Invalid file '{}'".format(path))
+
+    user_inp = input("Please input your location (lat, lon): ")
+    try:
+        (user_lat, user_lon) = list(map(float, user_inp.split(",")))
+    except ValueError:
+        sys.exit('Invalid input')
+
+    closest = get_closest_bar(bars, user_lon, user_lat)
+    print("Closest bar:", closest['properties']['Attributes']['Name'],
+          "Address:", closest['properties']['Attributes']['Address'])
+
+    biggest = get_biggest_bar(bars)
+    seats_count = biggest['properties']['Attributes']['SeatsCount']
+    print("Biggest bar:", biggest['properties']['Attributes']['Name'],
+          "Seats count:", seats_count)
+
+    smallest = get_smallest_bar(bars)
+    seats_count = smallest['properties']['Attributes']['SeatsCount']
+    print("Smallest bar:", smallest['properties']['Attributes']['Name'],
+          "Seats count:", seats_count)
