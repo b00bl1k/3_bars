1b6f2fe1eb90e73624a9b10a325b36abdcec20db
Alexey Ryabov <6006l1k@gmail.com>
2019-02-21 21:08:40 +0300

    Decrease nested levels and rename the data variable

--- bars.py
+++ bars.py
@@ -5,23 +5,20 @@ from geopy.distance import distance
 
 def load_data(filepath):
     with open(filepath, 'r', encoding="utf8") as file_handler:
-        return json.load(file_handler)
+        return json.load(file_handler)['features']
 
 
-def get_biggest_bar(data):
-    features = data['features']
-    return max(features,
+def get_biggest_bar(bars):
+    return max(bars,
                key=lambda kv: kv['properties']['Attributes']['SeatsCount'])
 
 
-def get_smallest_bar(data):
-    features = data['features']
-    return min(features,
+def get_smallest_bar(bars):
+    return min(bars,
                key=lambda kv: kv['properties']['Attributes']['SeatsCount'])
 
 
-def get_closest_bar(data, longitude, latitude):
-    features = data['features']
+def get_closest_bar(bars, longitude, latitude):
     user_loc = (latitude, longitude)
 
     def sort_by_distance(kv):
@@ -29,38 +26,34 @@ def get_closest_bar(data, longitude, latitude):
         bar_loc = (lat, lon)
         return distance(bar_loc, user_loc).km
 
-    sorted_by_distance = sorted(features, key=sort_by_distance)
+    sorted_by_distance = sorted(bars, key=sort_by_distance)
     return sorted_by_distance[0]
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        sys.exit("Invalid command line")
-
-    path = sys.argv[1]
     try:
-        bars = load_data(path)
+        bars_list = load_data(sys.argv[1])
+        user_inp = input("Please input your location (lat, lon): ")
+        (user_lat, user_lon) = list(map(float, user_inp.split(",")))
     except FileNotFoundError:
         sys.exit("File '{}' not found".format(path))
     except json.JSONDecodeError:
         sys.exit("Invalid file '{}'".format(path))
-
-    user_inp = input("Please input your location (lat, lon): ")
-    try:
-        (user_lat, user_lon) = list(map(float, user_inp.split(",")))
     except ValueError:
         sys.exit('Invalid input')
+    except IndexError:
+        sys.exit("Invalid command line arguments")
 
-    closest = get_closest_bar(bars, user_lon, user_lat)
+    closest = get_closest_bar(bars_list, user_lon, user_lat)
     print("Closest bar:", closest['properties']['Attributes']['Name'],
           "Address:", closest['properties']['Attributes']['Address'])
 
-    biggest = get_biggest_bar(bars)
+    biggest = get_biggest_bar(bars_list)
     seats_count = biggest['properties']['Attributes']['SeatsCount']
     print("Biggest bar:", biggest['properties']['Attributes']['Name'],
           "Seats count:", seats_count)
 
-    smallest = get_smallest_bar(bars)
+    smallest = get_smallest_bar(bars_list)
     seats_count = smallest['properties']['Attributes']['SeatsCount']
     print("Smallest bar:", smallest['properties']['Attributes']['Name'],
           "Seats count:", seats_count)
